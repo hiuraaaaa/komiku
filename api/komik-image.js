0793a3d7ad496922814bf33a08b9fde676629716
Hiura <hiura0012@gmail.com>
2025-12-15 22:16:41 +0700

    Create komik-image.js.

---
+++ api/komik-image.js
@@ -0,0 +1,11 @@
+export default async function handler(req,res){
+  const { url }=req.query;
+  if(!url) return res.status(400).json({success:false,message:"URL required"});
+  try{
+    const response=await fetch(`https://api.nekolabs.web.id/discovery/komikindo/v2/get-image?url=${encodeURIComponent(url)}`);
+    const data=await response.json();
+    res.status(200).json(data);
+  }catch(err){
+    res.status(500).json({success:false,message:"Gagal load gambar chapter"});
+  }
+}
